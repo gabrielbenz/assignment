653a859f95e27c0eb995f4744d07614f3380c70f
Gabriel Benz <glbenz@gmail.com>
2018-11-21 12:22:14 -0200

    Trying to expose rest api with spring integration.

--- src/main/java/com/geocoding/assignment/AssignmentApplication.java
+++ src/main/java/com/geocoding/assignment/AssignmentApplication.java
@@ -1,3 +1,6 @@
+package com.geocoding.assignment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -17,5 +20,21 @@ public class AssignmentApplication {
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
 
+	/**
+	 * Exposes rest api GET method /api/geocode?{address} 
+	 * then publish the json formatted message to a JMS queue 
+	 * (activeMQ component). Finally, the response is converted 
+	 * into json and returned to the http requester.
+	 * 
+	 * @return IntegrationFlow - the integration flow.
+	 */
+	@Bean
+	public IntegrationFlow restApiInputFlow() {
+		return IntegrationFlows.from(Http.inboundGateway("/api/geocode").requestMapping(rm -> rm.methods(HttpMethod.GET))
+					.requestPayloadType(String.class).payloadExpression("#requestParams['address']")
+				)
+				.transform(Transformers.toJson())
+				.get();
+	}
 
 }
